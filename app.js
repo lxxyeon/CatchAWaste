d944f08adb538fa24ce16903bad0439175232269
안이연 <lxxyeon@gmail.com>
2024-12-16 16:58:20 +0900

    node.js setting init

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+    console.log('connect /')
+    res.send('Hello World !!')
+})
+app.listen(3000, function () {
+    console.log('3000 port listen !!')
+})
